64592223215fdd1a5b35067d7e5b61a9b521597a
Kari Sullivan <karisullivan@karis-air.lan>
2023-11-07 17:10:27 -0600

    added defaults

--- latte.py
+++ latte.py
@@ -15,22 +15,41 @@ print ("You spend", round(year_cost,2), "a year on", splurge)
 print("How many years until you retire?")
 investment_years = float(input())
 
-print("What's your expected rate of return (eg: 0.1)?")
-rate = float(input())
+print("Optional: What's your expected rate of return (eg: 0.1)?")
+
+rate = input()
+
+if rate == '':
+    rate = 0.1
+else:
+    rate = float(rate)
+
 
 total_earned = year_cost * (1+rate)**investment_years
 
 print ("You would earn", round(total_earned, 2), "investing the money you spend on", splurge, "in a single year")
 
-print("What's your expected withdrawl rate (eg: 0.04)?")
-withdrawl_rate = float(input())
+print("Optional: What's your expected withdrawl rate (eg: 0.04)?")
+withdrawl_rate = input()
+
+if withdrawl_rate == '':
+    withdrawl_rate = 0.04
+else:
+    withdrawl_rate = float(withdrawl_rate)
+
 
 yearly_surplus = withdrawl_rate * total_earned
 
 print ("You would earn", round(yearly_surplus, 2), "a year in retirement on this", splurge, "money")
 
-print("What is the rate of inflation (eg: 0.05)?")
-inflation_rate = float(input())
+print("Optional: What is the rate of inflation (eg: 0.05)?")
+
+inflation_rate = input()
+
+if inflation_rate == '':
+    inflation_rate = 0.05
+else:
+    inflation_rate = float(inflation_rate)
 
 future_cost = cost * (1+inflation_rate)**investment_years
 
